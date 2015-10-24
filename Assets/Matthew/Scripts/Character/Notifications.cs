c33d4b5e0e4b94a9c25efe75929129db64cf3ce7
Matthew Hatcher <hello@matthewh.in>
2015-10-24 14:31:01 -0400

    Adding scripts, prefabs, updating Manor

---
+++ Assets/Matthew/Scripts/Character/Notifications.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Notifications : MonoBehaviour
+{
+    public Text NotifyText;
+    public void Notify(string msg)
+    {
+        NotifyText.text = msg;
+    }
+
+    public void ResetNotify()
+    {
+        NotifyText.text = "";
+    }
+}
---
+++ Assets/Matthew/Scripts/Lighting/WallLamp.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallLamp : MonoBehaviour
+{
+    private Light LightComponent;
+	void Start ()
+    {
+        LightComponent = GetComponent<Light>();
+        StartCoroutine("FlickerOff");
+    }
+	
+    IEnumerator FlickerOff()
+    {
+        while (true)
+        {
+            for (int i = 0; i < Random.Range(5, 15); i++)
+            {
+                LightComponent.enabled = false;
+                yield return new WaitForSeconds(Random.Range(0.1f, 0.4f));
+                LightComponent.enabled = true;
+                yield return new WaitForSeconds(Random.Range(0.1f, 0.4f));
+            }
+            LightComponent.enabled = false;
+            yield return new WaitForSeconds(Random.Range(1, 10));
+            StartCoroutine("FlickerOn");
+        }
+    }
+
+    IEnumerator FlickerOn()
+    {
+        
+        for (int i = 0; i < Random.Range(7, 20); i++)
+        {
+            LightComponent.enabled = true;
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.4f));
+            LightComponent.enabled = false;
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.4f));
+        }
+        LightComponent.enabled = true;
+        yield return new WaitForSeconds(Random.Range(1, 10));
+        StartCoroutine("FlickerOff");
+    }
+
+}
