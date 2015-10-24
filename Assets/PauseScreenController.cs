edd23a16aa5af8ef01fcad5c5d79feb4aaa9fc1d
baocin <steele.pedersen@gmail.com>
2015-10-24 18:10:56 -0400

    Pause Menu and Title Screen Introduction - underlying code needs to be written

---
+++ Assets/PauseScreenController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScreenController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void exitGame(){
+		Application.Quit();
+	}
+
+	public void exitPause(){
+		this.gameObject.SetActive = false;
+	}
+
+	public void openSettings(){
+		UnityEditor.ResolutionDialogSetting ();
+	}
+}
