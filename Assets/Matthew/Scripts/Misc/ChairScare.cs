faf04512ed2a87dbee75615db12eea0575480b31
Matthew Hatcher <hello@matthewh.in>
2015-10-24 23:08:58 -0400

    Surprise!

--- Assets/Matthew/Scripts/Misc/ChairScare.cs
+++ Assets/Matthew/Scripts/Misc/ChairScare.cs
@@ -6,7 +6,7 @@ public class ChairScare : MonoBehaviour
     bool hasScared = false;
     AudioSource _audio;
     public AudioClip Roar, Scream, Thunder;
-    public GameObject Bolter;
+    public GameObject Hanger;
     public GameObject Lightning, Atmoshpere;
 
     void Awake()
@@ -20,26 +20,35 @@ public class ChairScare : MonoBehaviour
         Debug.Log("Item has entered");
         if (obj.tag == "Player")
         {
-            _audio.PlayOneShot(Thunder, 0.4f);
-            Bolter.SetActive(true);
-            _audio.PlayOneShot(Roar, 0.7f);
-            _audio.PlayOneShot(Scream, 0.5f);
-            StartCoroutine("Flicker");
+            if (!hasScared)
+            {
+                _audio.PlayOneShot(Thunder, 0.4f);
+                Hanger.SetActive(true);
+                _audio.PlayOneShot(Roar, 0.7f);
+                _audio.PlayOneShot(Scream, 0.5f);
+                StartCoroutine("Flicker");
+            }
         }
     }
 
     IEnumerator Flicker()
     {
-        while (true)
+        for (int i = 0; i < Random.Range(3, 6); i++)
         {
-            for (int i = 0; i < Random.Range(5, 15); i++)
-            {
-                Debug.Log("Running");
-                Lightning.SetActive(true);
-                yield return new WaitForSeconds(Random.Range(0.1f, 0.4f));
-                Lightning.SetActive(false);
-                yield return new WaitForSeconds(Random.Range(0.1f, 0.4f));
-            }
+            Lightning.SetActive(true);
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.4f));
+            Lightning.SetActive(false);
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.4f));
         }
+        yield return new WaitForSeconds(2.25f);
+        ReturnToNorm();
+    }
+
+    void ReturnToNorm()
+    {
+        hasScared = true;
+        Lightning.SetActive(false);
+        _audio.Stop();
+        Hanger.SetActive(false);
     }
 }
