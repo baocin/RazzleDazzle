7f3aace72eca5477955aa3d230f488cf36e0e09d
Matthew Hatcher <hello@matthewh.in>
2015-10-25 16:18:42 -0400

    Title screen shake

---
+++ Assets/Matthew/Scripts/Misc/TitleScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour
+{
+    public Camera cam;
+    public float shake = 0f, shakeAmount = 0.7f, decreaseFactor = 1.0f;
+
+    void Update()
+    {
+        if (shake > 0)
+        {
+            cam.transform.localPosition = new Vector3((Random.insideUnitSphere.x * shakeAmount), (float)(Random.insideUnitSphere.y * shakeAmount));
+            shake -= Time.deltaTime * decreaseFactor;
+        }
+        if (shakeAmount > 25)
+        {
+            shakeAmount += 0.15f * Time.deltaTime;
+        }
+    }
+}
