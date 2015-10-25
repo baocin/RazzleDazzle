47914abd52c94e63c7e27644b873d330fafc08c2
Matthew Hatcher <hello@matthewh.in>
2015-10-25 17:21:34 -0400

    Adding Bijan folder

---
+++ Assets/Bijan/Scripts/LeftArmRotation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftArmRotation : MonoBehaviour {
+
+	const float armRotation = 5f;
+
+	void Update () {
+		gameObject.GetComponent<Rigidbody>().rotation = new Quaternion(armRotation,0,0,0);
+	}
+}
