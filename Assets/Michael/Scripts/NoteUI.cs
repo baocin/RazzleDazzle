a1d9c88bdac8483af29fc90470924a33041bc9a0
baocin <steele.pedersen@gmail.com>
2015-10-25 05:20:05 -0400

    Basic Notes

---
+++ Assets/Michael/Scripts/NoteUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoteUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnClick(){
+		Debug.Log ("Clicked outside of note - closing");
+		GameObject.Find ("NoteImage").SetActive (false);
+	}
+}
